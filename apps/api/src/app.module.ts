4679fc4b137dc63594a6844aae56c1f7370385f0
Francis Molina <francisjmp99@gmail.com>
2021-05-24 20:50:01 +0200

    chore: added modules to app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,13 +3,23 @@ import { DynamicModule, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { AppLoggerMiddleware } from './app.middleware';
 import { AuthModule } from './auth/auth.module';
 import { BootstrapModule } from './bootstrap.module';
+import { CompetitionModule } from './competition/infrastructure';
+import { MatchModule } from './match/infrastructure';
+import { TeamModule } from './team/infrastructure';
 import { UserModule } from './user/infrastructure';
 
 export class AppModule implements NestModule {
   static forRoot(): DynamicModule {
     return {
       module: this,
-      imports: [BootstrapModule, AuthModule, UserModule],
+      imports: [
+        BootstrapModule,
+        AuthModule,
+        UserModule,
+        CompetitionModule,
+        TeamModule,
+        MatchModule,
+      ],
     };
   }
 

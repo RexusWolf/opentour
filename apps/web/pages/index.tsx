f603bdcd0ee4f6afc9dd98f79afcccef307cb55e
Francis Molina <francisjmp99@gmail.com>
2021-08-08 10:48:13 +0200

    chore: conditional rendering of login page

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,13 +1,17 @@
 import { Homepage, Layout } from '@opentour/ui';
+import hi from 'date-fns/esm/locale/hi/index.js';
 import { useSession } from 'next-auth/client';
-import React from 'react';
+import { useRouter } from 'next/dist/client/router';
+import React, { useEffect } from 'react';
 
 export default function Index() {
+  const router = useRouter();
   const [session, loading] = useSession();
 
-  return (
-    <Layout session={session!}>
-      <Homepage />
-    </Layout>
-  );
+  useEffect(() => {
+    if (!loading && !session) {
+      router.push('/signIn');
+    }
+  }, [loading, session, router]);
+  return <Layout session={session!}>{session && <Homepage />}</Layout>;
 }
